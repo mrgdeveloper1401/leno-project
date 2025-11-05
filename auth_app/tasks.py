05fdac50f9077a9fb9d32761084ba02df7715a36
mohammad goodarzi <123094892+mrgdeveloper1401@users.noreply.github.com>
2025-11-05 14:02:54 +0000

    bug fix update refresh_token

--- auth_app/tasks.py
+++ auth_app/tasks.py
@@ -5,12 +5,22 @@ from auth_app.clasess import AuthService
 from auth_app.models import UserToken
 
 
+import time
+import asyncio
+from celery import shared_task
+from auth_app.clasess import AuthService
+from auth_app.models import UserToken
+
+
 @shared_task(queue="refresh_token", max_retries=3)
 def refresh_expiring_tokens():
     now_ts = int(time.time())
     expired_now_ts = now_ts + 30 * 60
 
-    user_tokens = UserToken.objects.filter(expire_in_timestamp__lte=expired_now_ts)
+    user_tokens = UserToken.objects.filter(
+        expire_in_timestamp__lte=expired_now_ts
+    ).select_related('user')
+    # user_tokens = UserToken.objects.select_related('user')
 
     if not user_tokens:
         return
@@ -21,28 +31,29 @@ def refresh_expiring_tokens():
         tasks = []
 
         async def refresh_single_token(token_obj):
-            response = await auth.rotate_token(token_obj.access_token, token_obj.refresh_token)
+            user_id = token_obj.user_id
 
+            response = await auth.rotate_token(token_obj.access_token, token_obj.refresh_token)
             if str(response.get("Success")).lower() == "true":
                 result = response['Result']
                 new_access = result['access_token']
                 new_refresh = result['refresh_token']
                 new_expire = int(result['user']['expire_in_timestamp'])
 
-                UserToken.objects.filter(user_id=token_obj.user.id).update(
+                await UserToken.objects.filter(user_id=user_id).aupdate(
                     access_token=new_access,
                     refresh_token=new_refresh,
                     expire_in_timestamp=new_expire,
                 )
 
-        # create list for coroutine
-        for i in user_tokens:
-            tasks.append(refresh_single_token(i))
+        for token in user_tokens:
+            tasks.append(refresh_single_token(token))
 
-        # run task
         await asyncio.gather(*tasks)
 
     asyncio.run(process_tokens())
+
+
 
 
 @shared_task(queue="logout_user", max_retries=3)
